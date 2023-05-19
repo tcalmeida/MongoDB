d02916a11d408483932ffa10995d7dd6de2ff33f
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-05-19 15:42:24 -0300

    refactor: change get route address

--- src/modules/posts/routes.ts
+++ src/modules/posts/routes.ts
@@ -1,10 +1,10 @@
-import { Router } from "express";
-import controller from "./controller";
+import { Router } from 'express';
+import controller from './controller';
 
 const routes = Router();
 
-routes.post("/post", controller.create);
-routes.get("/post", controller.findAll);
+routes.post('/post', controller.create);
+routes.get('/', controller.findAll);
 //routes.get("/post/:id", controller.findOne);
 //routes.put("/post/:id", controller.update);
 //routes.delete("/post/:id", controller.delete);
