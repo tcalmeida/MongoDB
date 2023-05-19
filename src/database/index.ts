a745a28882825926f968f00085e8e9272a84f9b1
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-05-19 15:45:15 -0300

    fix: change database name

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import Connection from "./Conection";
 
-const mongoDB = new Connection("mongodb://127.0.0.1:27017/treinamento"); // insert localhost: 127.0.0.1 and database name
+const mongoDB = new Connection("mongodb://127.0.0.1:27017/blog");
 
 export { mongoDB };
