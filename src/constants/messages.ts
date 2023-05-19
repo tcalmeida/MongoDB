350063481ba62185087ea927a685649037e1a1a8
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-05-19 19:56:24 -0300

    build: create message error constants

---
+++ src/constants/messages.ts
@@ -0,0 +1,13 @@
+const MESSAGE = {
+  ERROR: {
+    SEARCH_DB: 'Unable to perform this action',
+    ID_NOT_FOUND: 'ID not found',
+    UPDATE_REGISTER: 'Unable to update data',
+    DELETE: 'Unable to delete data',
+    REGISTER: {
+      USER: 'Unable to register user',
+    },
+  },
+};
+
+export default MESSAGE;
