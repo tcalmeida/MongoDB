afd248d4a74c897b1359eaf12bd7ccadb677dfe7
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-05-19 16:11:10 -0300

    refactor: add static properties && message error constants

--- src/modules/users/controller.ts
+++ src/modules/users/controller.ts
@@ -1,55 +1,71 @@
 import { Request, Response } from 'express';
 import { User } from '../../models';
+import MESSAGE from '../../constants/messages';
 
-const controller = {
-  async create(req: Request, res: Response) {
-    const { name, email, password } = req.body;
+class controller {
+  static create = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { name, email, password } = req.body;
+      const newUser = await User.create({
+        name,
+        email,
+        password,
+      });
+      return res.status(201).json(newUser);
+    } catch (error) {
+      return res.status(400).json({ message: MESSAGE.ERROR.REGISTER.USER });
+    }
+  }
 
-    const newUser = await User.create({
-      name,
-      email,
-      password,
-    });
-    return res.status(201).json(newUser);
-  },
+  static findAll = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const users = await User.find();
+      return res.json(users);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.SEARCH_DB });
+    }
+  }
 
-  async findAll(req: Request, res: Response) {
-    const users = await User.find();
+ static findOne = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const user = await User.findById(id);
+      return res.json(user);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.SEARCH_DB });
+    }
+  }
 
-    return res.json(users);
-  },
-
-  async findOne(req: Request, res: Response) {
-    const { id } = req.params;
-    const user = await User.findById(id);
-
-    return res.json(user);
-  },
-
-  async update(req: Request, res: Response) {
-    const { id } = req.params;
-    const { name, email } = req.body;
-    const updated = await User.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          name,
-          email,
+  static update = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const { name, email } = req.body;
+      await User.updateOne(
+        {
+          _id: id,
         },
-      }
-    );
+        {
+          $set: {
+            name,
+            email,
+          },
+        }
+      );
+      return res.sendStatus(204);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.UPDATE_REGISTER });
+    }
+  }
 
-    return res.sendStatus(204);
-  },
-
-  async delete(req: Request, res: Response) {
-    const { id } = req.params;
-    await User.findByIdAndDelete(id);
-
-    return res.sendStatus(204);
-  },
-};
+  static delete = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      await User.findByIdAndDelete(id);
+      return res.sendStatus(204);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.DELETE });
+    }
+  }
+}
 
 export default controller;
