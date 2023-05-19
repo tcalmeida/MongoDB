003f2df0a244cf76d996173f7aeb5876647ca2b4
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-05-19 19:55:53 -0300

    refactor: add static properties && message error constants

--- src/modules/posts/controller.ts
+++ src/modules/posts/controller.ts
@@ -1,58 +1,73 @@
-import { Request, Response } from "express";
-import { Post } from "../../models/";
+import { Request, Response } from 'express';
+import { Post } from '../../models/';
+import MESSAGE from '../../constants/messages';
 
-const controller = {
-  async create(req: Request, res: Response) {
-    const { user, conteudo } = req.body;
+class controller {
+  static create = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { user, content } = req.body;
+      const newPost = await Post.create({
+        user,
+        content,
+      });
+      return res.status(201).json(newPost);
+    } catch (error) {
+      return res.status(400).json({ message: MESSAGE.ERROR.REGISTER.USER });
+    }
+  };
 
-    const newPost = await Post.create({
-      user,
-      conteudo,
-    });
-    return res.status(201).json(newPost);
-  },
+  static findAll = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const posts = await Post.find().populate({
+        path: 'user',
+        select: 'name',
+      });
+      return res.json(posts);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.SEARCH_DB });
+    }
+  };
 
-  async findAll(req: Request, res: Response) {
-    //const posts = await Post.find().populate("user"); // populate: receive info of which document will be populate
-    const posts = await Post.find().populate({          // populate especific itens :receive info of which propriety and itens will be populate
-      path: "user",
-      select: "nome",
-    });
+  static findOne = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const post = await Post.findById(id);
+      return res.json(post);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.SEARCH_DB });
+    }
+  };
 
-    return res.json(posts);
-  },
-
-  /* async findOne(req: Request, res: Response) {
-    const { id } = req.params;
-    const post = await Post.findById(id);
-
-    return res.json(post);
-  },
-
-  async update(req: Request, res: Response) {
-    const { id } = req.params;
-    const { user, conteudo } = req.body;
-    const updated = await Post.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          user, 
-          conteudo
+  static update = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const { user, content } = req.body;
+      await Post.updateOne(
+        {
+          _id: id,
         },
-      }
-    );
+        {
+          $set: {
+            user,
+            content,
+          },
+        }
+      );
+      return res.sendStatus(204);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.UPDATE_REGISTER });
+    }
+  };
 
-    return res.sendStatus(204);
-  },
-  
-  async delete(req: Request, res: Response) {
-    const { id } = req.params;
-    await Post.findByIdAndDelete(id)
-
-    return res.sendStatus(204);
-  } */
-};
+  static delete = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      await Post.findByIdAndDelete(id);
+      return res.sendStatus(204);
+    } catch (error) {
+      return res.status(500).json({ message: MESSAGE.ERROR.DELETE });
+    }
+  };
+}
 
 export default controller;
